defc7e9078258e1f682aea3f3d40db7254b39bce
Kevin Pickett <kevin.pickett@aggiemail.usu.edu>
2020-11-12 17:26:01 -0700

    Create index.js

---
+++ index.js
@@ -0,0 +1,21 @@
+var app = new Vue({
+  el: '#app',
+  data: {
+    date: "Sunday, November 15th",
+    link: "https://www.youtube.com/embed/HSndhYOUyAY",
+    meetings: [
+      { name: 'Example', link: 'https://www.churchofjesuschrist.org/?lang=eng' }
+    ]
+  }
+})
+
+/*
+    // Example Data
+    data: {
+        date: "Sunday, November 15th",
+        link: "https://www.youtube.com/embed/HSndhYOUyAY",
+        meetings: [
+            { name: 'Example', link: 'https://www.churchofjesuschrist.org/?lang=eng' }
+        ]
+    }
+*/
