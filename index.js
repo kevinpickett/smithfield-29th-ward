be6f4efa4aed3156a92d891d6f9f3d6b86d3ffe5
Kevin Pickett <kevin.pickett@aggiemail.usu.edu>
2020-11-13 09:05:08 -0700

    Update index.js
    Removed pwd from url

--- index.js
+++ index.js
@@ -4,8 +4,8 @@ var app = new Vue({
     date: "Sunday, November 15th",
     link: "https://www.youtube.com/embed/22IbsrQCkKU",
     meetings: [
-      { name: 'Adult Sunday School @ 2:45', link: 'https://us05web.zoom.us/j/86917141620?pwd=OWdmV3FHZ1VmaVlkdW9lN0lMSlhoQT09' },
-      { name: 'Youth Sunday School @ 3:30', link: 'https://us05web.zoom.us/j/89979724476?pwd=bkJZeTZkM0ptRTJBaC9JR1RRSGVLQT09' }
+      { name: 'Adult Sunday School @ 2:45', link: 'https://us05web.zoom.us/j/86917141620' },
+      { name: 'Youth Sunday School @ 3:30', link: 'https://us05web.zoom.us/j/89979724476' }
     ]
   }
 })
