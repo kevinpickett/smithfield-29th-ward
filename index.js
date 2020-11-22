e50e3c59edd0d7cf953c4822412b179df1cbafbc
Kevin Pickett <kevin.pickett@aggiemail.usu.edu>
2020-11-22 08:11:56 -0700

    Update index.js

--- index.js
+++ index.js
@@ -5,6 +5,7 @@ var app = new Vue({
     link: "https://www.youtube.com/embed/9do6O2gztO8",
     meetings: [
       { name: 'Relief Society @ 2:45', link: 'https://us05web.zoom.us/j/86249881519?pwd=QTFyZEl0eS8yOWpMaGdNRVJUSXhiQT09' },
+      { name: 'Elders Quorum @ 3:30', link: 'https://us02web.zoom.us/j/87329902416?pwd=Q2xDejR3YUJQR3B1UnlvcUYrWlpKUT09' },
       { name: 'Stake Devotional @ 7:00', link: 'http://mywebcast.churchofjesuschrist.org/smithfieldutahsouthstake' }
     ]
   }
