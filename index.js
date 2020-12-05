9782811541e5359eecdf749f4a7f1b612e8ba762
Kevin Pickett <kevin.pickett@aggiemail.usu.edu>
2020-12-05 12:26:20 -0700

    Update index.js

--- index.js
+++ index.js
@@ -1,11 +1,12 @@
 var app = new Vue({
   el: '#app',
   data: {
-    date: "Sunday, November 29th",
-    link: "https://youtu.be/3D5fIMKOy0g",
-    embed: "https://www.youtube.com/embed/3D5fIMKOy0g",
+    date: "December 6th",
+    link: "https://youtu.be/dOkfSVHtWIE",
+    embed: "https://www.youtube.com/embed/dOkfSVHtWIE",
     meetings: [
-      { name: 'Sunday School - 5th Sunday Lesson @ 2:45', link: 'https://us05web.zoom.us/j/85335051983?pwd=Qng0OGFsa0ZIY1NzV0FKaEJFaW9Hdz09' }
+      { name: 'Adult Sunday School @ 2:45', link: 'https://us05web.zoom.us/j/86917141620?pwd=OWdmV3FHZ1VmaVlkdW9lN0lMSlhoQT09' },
+      { name: 'Youth Sunday School @ 3:30', link: 'https://us05web.zoom.us/j/89979724476?pwd=bkJZeTZkM0ptRTJBaC9JR1RRSGVLQT09' }
     ],
     modalState: false
   }
@@ -22,6 +23,3 @@ var app = new Vue({
         ]
     }
 */
-
-// 11/29: https://www.youtube.com/embed/3D5fIMKOy0g
-// 12/6:  https://www.youtube.com/embed/dOkfSVHtWIE
