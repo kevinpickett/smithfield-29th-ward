09860fc2f6a2c050075f45a10833e169ff9234d9
Kevin Pickett <kevin.pickett@aggiemail.usu.edu>
2021-02-20 07:33:57 -0700

    Update index.js

--- index.js
+++ index.js
@@ -1,46 +1,46 @@
 var app = new Vue({
   el: '#app',
   data: {
-    link: "https://youtu.be/pYpUd99Ab6k",
-    singingTime: "https://youtu.be/vLUH8Q7VLsU",
-    embed: "https://www.youtube.com/embed/pYpUd99Ab6k",
+    link: "https://youtu.be/FaqWjkgwQEY",
+    embed: "https://www.youtube.com/embed/FaqWjkgwQEY",
+    singingTime: "", // last = https://youtu.be/vLUH8Q7VLsU
     // 'date' is now a computed property based on the schedule (bishop requested we display a schedule with the group that should attend)
     // schedule[0] should be for the current upcoming Sunday
     schedule: [
-      { date: 'February 14th', group: 'A-K' },
       { date: 'February 21st', group: 'L-Z' },
       { date: 'February 28th', group: 'A-K' },
       { date: 'March 7th', group: 'L-Z' },
+      { date: 'March 14th', group: 'A-K' },
     ],
     // Note is optional. If there is no note,  remove the note property/value.
     meetings: [
-//         { 
-//           name: 'Gospel Doctrine @ 10:15',
-//           link: 'https://zoom.us/j/98479458051?pwd=a2l5d1lnSHZKSWtJYlNUc0lraWlVUT09',
-//           type: 'zoom',
-//           note: {
-//             title: 'Gospel Doctrine', 
-//             text: 'Meeting ID: "984 7945 8051" Passcode: "068265".'
-//           }
-//         },
-//       { 
-//         name: 'Youth Sunday School 11-13 @ 11:00', 
-//         link: 'https://us05web.zoom.us/j/82813398554?pwd=cXBDenhUZklWZDlCK1VocklZbkhjQT09',
-//         type: 'zoom', 
-//         note: {
-//           title: 'Youth Sunday School 11-13', 
-//           text: 'Meeting ID: "828 1339 8554" Passcode: "QT8MCu".' 
-//         }
-//       },
-//       { 
-//         name: 'Youth Sunday School 14-17 @ 11:00', 
-//         link: 'https://us05web.zoom.us/j/86327485143?pwd=TURJb2NtYXhudTNwYWJFTkJOR3NzUT09',
-//         type: 'zoom', 
-//         note: {
-//           title: 'Youth Sunday School 14-17', 
-//           text: 'Meeting ID: "863 2748 5143" Passcode: "4b2ft6".' 
-//         }
-//       },
+        { 
+          name: 'Gospel Doctrine @ 10:15',
+          link: 'https://zoom.us/j/98479458051?pwd=a2l5d1lnSHZKSWtJYlNUc0lraWlVUT09',
+          type: 'zoom',
+          note: {
+            title: 'Gospel Doctrine', 
+            text: 'Meeting ID: "984 7945 8051" Passcode: "068265".'
+          }
+        },
+      { 
+        name: 'Youth Sunday School 11-13 @ 11:00', 
+        link: 'https://us05web.zoom.us/j/82813398554?pwd=cXBDenhUZklWZDlCK1VocklZbkhjQT09',
+        type: 'zoom', 
+        note: {
+          title: 'Youth Sunday School 11-13', 
+          text: 'Meeting ID: "828 1339 8554" Passcode: "QT8MCu".' 
+        }
+      },
+      { 
+        name: 'Youth Sunday School 14-17 @ 11:00', 
+        link: 'https://us05web.zoom.us/j/86327485143?pwd=TURJb2NtYXhudTNwYWJFTkJOR3NzUT09',
+        type: 'zoom', 
+        note: {
+          title: 'Youth Sunday School 14-17', 
+          text: 'Meeting ID: "863 2748 5143" Passcode: "4b2ft6".' 
+        }
+      },
 //       {
 //          name: '5th Sunday Lesson @ 10:15 am', 
 //          link: 'https://zoom.us/j/98479458051?pwd=a2l5d1lnSHZKSWtJYlNUc0lraWlVUT09',
@@ -50,29 +50,29 @@ var app = new Vue({
 //            text: 'Meeting ID: "984 7945 8051" Passcode: "068265"' 
 //          }
 //        },
-      { 
-         name: 'Elders Quorum @ 10:15 am', 
-         link: 'https://zoom.us/j/96205487511?pwd=L3B1VTFQVVhEdU5Vd2hubzhEcVZMdz09',
-         type: 'zoom', 
-         note: {
-           title: 'Elders Quorum Meeting', 
-           text: 'Meeting ID: "962 0548 7511" Passcode: "478813"' 
-         }
-       },
-      { 
-         name: 'Relief Society @ 11:00 am', 
-         link: 'https://zoom.us/j/97569151520?pwd=WTlPRy9GV2ZxS1dKUWdiYzFodmxUZz09',
-         type: 'zoom', 
-         note: {
-           title: 'Relief Society', 
-           text: 'Meeting ID: "975 6915 1520" Passcode: "021383"' 
-         }
-       },
-       { 
-         name: 'Primary Singing Time', 
-         link: 'https://youtu.be/vLUH8Q7VLsU',
-         type: 'youtube', 
-       },
+//       { 
+//          name: 'Elders Quorum @ 10:15 am', 
+//          link: 'https://zoom.us/j/96205487511?pwd=L3B1VTFQVVhEdU5Vd2hubzhEcVZMdz09',
+//          type: 'zoom', 
+//          note: {
+//            title: 'Elders Quorum Meeting', 
+//            text: 'Meeting ID: "962 0548 7511" Passcode: "478813"' 
+//          }
+//        },
+//       { 
+//          name: 'Relief Society @ 11:00 am', 
+//          link: 'https://zoom.us/j/97569151520?pwd=WTlPRy9GV2ZxS1dKUWdiYzFodmxUZz09',
+//          type: 'zoom', 
+//          note: {
+//            title: 'Relief Society', 
+//            text: 'Meeting ID: "975 6915 1520" Passcode: "021383"' 
+//          }
+//        },
+//        { 
+//          name: 'Primary Singing Time', 
+//          link: 'https://youtu.be/vLUH8Q7VLsU',
+//          type: 'youtube', 
+//        },
     ],
     modalState: false,
     modalType: ''
