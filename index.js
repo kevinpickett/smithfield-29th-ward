2f7ff17deb97f43f14e5607e002d1ad4e1ec343e
Kevin Pickett <kevin.pickett@aggiemail.usu.edu>
2021-02-27 10:05:07 -0700

    Update index.js

--- index.js
+++ index.js
@@ -1,8 +1,6 @@
 var app = new Vue({
   el: '#app',
   data: {
-//     link: "https://youtu.be/OZlTH63N0ec",
-//     embed: "https://www.youtube.com/embed/OZlTH63N0ec",
     singingTime: "", // last = https://youtu.be/vLUH8Q7VLsU
     // 'date' is now a computed property based on the schedule (bishop requested we display a schedule with the group that should attend)
     // schedule[0] should be for the current upcoming Sunday
@@ -13,7 +11,6 @@ var app = new Vue({
       { date: 'March 21st', group: 'L-Z', meetingType: "school", link: "8AnVBbI0eTo" },
       { date: 'March 28th', group: 'A-K', meetingType: "grouping", link: "oJt3miShijA" },
     ],
-    // Note is optional. If there is no note,  remove the note property/value.
     sundaySchoolMeetings: [
         { 
           name: 'Gospel Doctrine @ 10:15',
@@ -95,11 +92,11 @@ var app = new Vue({
       return count > 0
     },
     sacramentLink: function() {
-      let link = "https://youtu.be/" this.schedule[0].link
+      let link = "https://youtu.be/" + this.schedule[0].link
       return link
     },
     sacramentEmbed: function() {
-      let embed = "https://youtu.be/embed/" this.schedule[0].link
+      let embed = "https://youtu.be/embed/" + this.schedule[0].link
       return embed
     },
     meetings: function() {
@@ -126,37 +123,3 @@ var app = new Vue({
     }
   }
 })
-
-/*
-    // Example Data
-    data: {
-      link: "https://youtu.be/dOkfSVHtWIE",
-      embed: "https://www.youtube.com/embed/dOkfSVHtWIE",
-      // date is now a computed property based on the schedule
-      schedule: [
-        { date: 'December 6th', group: '(A-K)' },
-        { date: 'December 13th', group: '(L-Z)' },
-        { date: 'December 20th', group: '(A-K)' },
-        { date: 'December 27th', group: '(L-Z)' },
-      ],
-      // Note is optional. If there is no note,  remove the no property/value
-      // In below example data meeting[0] has a note and meeting[1] does not have a note. Both are valid
-      meetings: [
-        { 
-          name: 'Adult Sunday School @ 2:45', 
-          link: 'https://us05web.zoom.us/j/86917141620?pwd=OWdmV3FHZ1VmaVlkdW9lN0lMSlhoQT09',
-          type: 'zoom', 
-          note: {
-            title: 'Adult Sunday School', 
-            text: 'If the one click link does not let you in, the meeting ID is: "853 3505 1983" and the password is: "837xks".' 
-          }
-        },
-        { 
-          name: 'Youth Sunday School @ 3:30', 
-          link: 'https://us05web.zoom.us/j/89979724476?pwd=bkJZeTZkM0ptRTJBaC9JR1RRSGVLQT09',
-          type: 'zoom', 
-        }
-      ],
-      modalState: false
-    }
-*/
